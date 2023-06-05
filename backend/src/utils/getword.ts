025637dd428084a7b990aabb3f54ebc368a20b42
OlliverLar <olliver.larsson@hotmail.com>
2023-06-05 16:54:38 +0200

    Corrected

--- backend/src/utils/getword.ts
+++ backend/src/utils/getword.ts
@@ -1,7 +1,7 @@
-export default function randomWord(allowRepeatedLetters: boolean, wordLength: number, wordList: string[]) {
-    const filteredWords = wordList.filter((word) => {
-      if (!allowRepeatedLetters) {
-        const letterCounts = {};
+export default function randomWord(allowDuplicates: boolean, wordLength: number, wordList: string[]) {
+  const filteredWords: string[] = wordList.filter((word) => {
+      if (!allowDuplicates) {
+        const letterCounts: Record<string, boolean> = {};
         for (let letter of word) {
           if (letterCounts[letter]) {
             return false;
